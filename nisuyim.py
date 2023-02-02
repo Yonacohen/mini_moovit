db059920dad972d439517caab3662c24ec9046e9
Yonacohen <73038461+Yonacohen@users.noreply.github.com>
2023-02-02 11:59:05 +0200

    update station

---
+++ nisuyim.py
@@ -0,0 +1,18 @@
+
+import Position1
+
+
+
+p0=Position1.Position(0,0)
+p1=Position1.Position(3,2)
+p2=Position1.Position(6,4)
+p3=Position1.Position(9,6)
+p4=Position1.Position(12,8)
+p5=Position1.Position(15,10)
+p6=Position1.Position(18,12)
+p7=Position1.Position(21,14)
+
+list_pos=[p0,p1,p2,p3,p4,p5,p6,p7]
+
+for p in list_pos:
+    p.print_pos()
